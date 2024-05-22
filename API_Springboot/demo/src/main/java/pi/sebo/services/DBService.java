5d1f61501a03d36341a9202c90fe63990f095485
Guilherme Picasso <guipicasso3@gmail.com>
2024-05-22 16:06:31 -0300

    query e correção

--- API_Springboot/demo/src/main/java/pi/sebo/services/DBService.java
+++ API_Springboot/demo/src/main/java/pi/sebo/services/DBService.java
@@ -23,9 +23,14 @@ public class DBService {
     @Bean
     public void instanciarBD(){
         Genero genero1 = new Genero("terror");
-		Genero genero2 = new Genero("ficção");
-		Genero genero3 = new Genero("drama");
-		generoRepository.saveAll(Arrays.asList(genero1,genero2,genero3));
+    Genero genero2 = new Genero("ficção");
+    Genero genero3 = new Genero("drama");
+    Genero genero4 = new Genero("biografia");
+    Genero genero5 = new Genero("ação");
+    Genero genero6 = new Genero("romance");
+    Genero genero7 = new Genero("aventura");
+    Genero genero8 = new Genero("autoajuda");
+    generoRepository.saveAll(Arrays.asList(genero1, genero2, genero3, genero4, genero5, genero6, genero7, genero8));
     }
     
 }
