c36952b770f6b8f3d3ca20fd9505493c13ef6343
AliePonzani <alinef.ponzani@gmail.com>
2024-05-23 00:43:02 -0300

    Front

---
+++ Front/FrontSebo/src/app/pages/login/login.component.ts
@@ -0,0 +1,52 @@
+import { Component } from '@angular/core';
+import { SharedModule } from '../../shared.module';
+import { FormGroup, FormControl } from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [SharedModule],
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+  loginForm: FormGroup;
+  valor: string = "";
+  valor2: string = "botão buscar não foi pressionado";
+  cont = 0;
+
+  hide = true;
+  clickEvent(event: MouseEvent) {
+    this.hide = !this.hide;
+    event.stopPropagation();
+  }
+
+  constructor(private router: Router) {
+    this.loginForm = new FormGroup({
+      usuario: new FormControl(''),
+      senha: new FormControl('')
+    });
+  }
+
+  onSubmit() {
+    const usuario = this.loginForm.value.usuario;
+    const senha = this.loginForm.value.senha;
+
+    if (usuario === "teste" && senha === "12345") {
+      this.router.navigate(['/home']);
+    } else {
+      this.valor = "Usuário ou senha inválidos!!!";
+    }
+
+   
+
+    // this.cont += 1;
+    // if (this.cont > 10) {
+    //   this.cont = 0;
+    // }
+    // this.valor = usuario;
+    // this.valor2 = "botão buscar foi pressionado " + this.cont + " vezes";
+
+  }
+}
